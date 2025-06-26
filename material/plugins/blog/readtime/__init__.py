9e9e05d7081cec24a84bbee6848171071b247bbc
jzstraley <107450982+jzstraley@users.noreply.github.com>
2025-06-25 22:01:26 -0400

    scss

--- material/plugins/blog/readtime/__init__.py
+++ material/plugins/blog/readtime/__init__.py
@@ -18,23 +18,34 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from mkdocs.config.base import Config
-from mkdocs.config.config_options import DictOfItems, Optional, SubConfig, Type
+import re
+
+from math import ceil
+
+from .parser import ReadtimeParser
 
 # -----------------------------------------------------------------------------
-# Classes
+# Functions
 # -----------------------------------------------------------------------------
 
-# Author
-class Author(Config):
-    name = Type(str)
-    description = Type(str)
-    avatar = Type(str)
-    slug = Optional(Type(str))
-    url = Optional(Type(str))
+# Compute readtime - we first used the original readtime library, but the list
+# of dependencies it brings with it increased the size of the Docker image by
+# 20 MB (packed), which is an increase of 50%. For this reason, we adapt the
+# original readtime algorithm to our needs - see https://t.ly/fPZ7L
+def readtime(html: str, words_per_minute: int):
+    parser = ReadtimeParser()
+    parser.feed(html)
+    parser.close()
 
-# -----------------------------------------------------------------------------
+    # Extract words from text and compute readtime in seconds
+    words = len(re.split(r"\W+", "".join(parser.text)))
+    seconds = ceil(words / words_per_minute * 60)
 
-# Authors
-class Authors(Config):
-    authors = DictOfItems(SubConfig(Author), default = {})
+    # Account for additional images
+    delta = 12
+    for _ in range(parser.images):
+        seconds += delta
+        if delta > 3: delta -= 1
+
+    # Return readtime in minutes
+    return ceil(seconds / 60)
